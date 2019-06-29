e8fba3e9d913a38915c8f2b67fac2f828e4f0cd3
Robert Worman <tr3ppy@gmail.com>
2019-06-29 16:59:49 +1000

    init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -cp target/uberjar/api.jar clojure.main -m api.core
---
+++ test/clj/api/test/handler.clj
@@ -0,0 +1,27 @@
+(ns api.test.handler
+  (:require
+    [clojure.test :refer :all]
+    [ring.mock.request :refer :all]
+    [api.handler :refer :all]
+    [api.middleware.formats :as formats]
+    [muuntaja.core :as m]
+    [mount.core :as mount]))
+
+(defn parse-json [body]
+  (m/decode formats/instance "application/json" body))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (mount/start #'api.config/env
+                 #'api.handler/app)
+    (f)))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= 200 (:status response)))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= 404 (:status response))))))
